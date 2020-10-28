fe3700e77d867df97726fab67f43ba65be5bf161
sudolaw <sunnyagreat@gmail.com>
2020-10-28 23:54:56 +0530

    directory tree

--- golang-router/authmod/authmod.go
+++ golang-router/authmod/authmod.go
@@ -1,6 +1,8 @@
 package authmod
 
 import (
+	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 
@@ -10,10 +12,23 @@ import (
 //Controller is for auth logic
 type Controller struct{}
 
+type Reqe struct {
+	PeerAddr     string
+	PeerPort     int
+	Mountpoint   string
+	Client       string
+	Username     string
+	Password     string
+	CleanSession bool
+}
+
 //Login is to process login request
 func (auth *Controller) Auth(c *gin.Context) {
 	body, _ := ioutil.ReadAll(c.Request.Body)
-	println(string(body))
+	j := string(body)
+	var t Reqe
+	json.Unmarshal([]byte(j), &t)
+	fmt.Print(t.Username)
 	c.JSON(http.StatusOK, gin.H{"result": "ok"})
 
 }
